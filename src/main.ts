9aa1c858c708eb1f111d04d65e9b708e3eb73673
Lucas Busso <lucasxbusso@gmail.com>
2024-06-18 19:30:57 -0300

    user CRUD services and user DTO

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
 import { CORS } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.setGlobalPrefix('api');
   app.enableCors(CORS);
   const configService = app.get(ConfigService);
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersEntity } from '../entities/user.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { UserDTO, UserUpdateDTO } from '../dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly userRepository: Repository<UsersEntity>,
+  ) {}
+
+  public async create(body: UserDTO): Promise<UsersEntity> {
+    try {
+      return await this.userRepository.save(body);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async findAll(): Promise<UsersEntity[]> {
+    try {
+      return await this.userRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async findById(id: string): Promise<UsersEntity> {
+    try {
+      return await this.userRepository
+        .createQueryBuilder('user')
+        .where({ id })
+        .getOne();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async update(
+    body: UserUpdateDTO,
+    id: string,
+  ): Promise<UpdateResult | undefined> {
+    try {
+      const user: UpdateResult = await this.userRepository.update(id, body);
+
+      if (user.affected === 0) {
+        return undefined;
+      } else {
+        return user;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async delete(id: string): Promise<DeleteResult> {
+    try {
+      const user: DeleteResult = await this.userRepository.delete(id);
+
+      if (user.affected === 0) {
+        return undefined;
+      } else {
+        return user;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
